179ff5f0b0f3a05753b09639d02635696aac367e
riesal <riesal@gmail.com>
2016-10-06 12:41:39 +0700

    Update massive-lock-users.sh

--- massive-lock-users.sh
+++ massive-lock-users.sh
@@ -11,6 +11,8 @@ function ch_pass_linux()
   passwd -l $i # set lock
   chage -E 0 $i # set expiration immediately
   echo -e "\tUser $i has been LOCKED successfuly\n"
+  locked_ok=$(grep $i /etc/shadow | cut -d':' -f2 | cut -d'*' -f2 | cut -c1-2)
+  locked_ok++
 }
 
 function ch_pass_sunos()
@@ -22,6 +24,7 @@ echo -e "This script will lock the unused users from this server"
 echo -e "It may take a while..\n"
 
 if [[ "$isLinux" == "Linux" ]]; then
+  isLinux=True
   for i in "${users[@]}"
   do
     ch_pass_linux
@@ -34,3 +37,17 @@ else
 fi
 
 echo -e "\nLocking completed, there are ${#users[@]} users in total."
+echo -e "\nList of locked users.. \n"
+
+if [[ $isLinux = True ]]; then
+  for i in "${users[@]}"
+  do
+    grep $i /etc/shadow | cut -d':' -f2 | cut -d'*' -f2 | cut -c1-2
+  done
+else
+  grep $i /etc/shadow | cut -d':' -f2 | cut -c1-2
+fi
+
+# grep $i /etc/shadow | cut -d':' -f2 | cut -d'*' -f2 | cut -c1-2
+# grep $i /etc/shadow | cut -d':' -f2 | cut -c1-2
+# declare -F ch_pass_linux &>/dev/null && echo "ch_pass_linux() found." || echo "ch_pass_linux() not found."
